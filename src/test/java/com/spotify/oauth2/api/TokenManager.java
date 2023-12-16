6fabd08e20002acfc157c0b7a54ad143674fe682
vasilevasluian <vasilevasluian@gmail.com>
2023-12-16 20:53:16 +0200

    first commit

---
+++ src/test/java/com/spotify/oauth2/api/TokenManager.java
@@ -0,0 +1,2 @@
+package com.spotify.oauth2.api;public class TokenManager {
+}
---
+++ src/test/java/com/spotify/oauth2/pojo/Error.java
@@ -0,0 +1,23 @@
+package com.spotify.oauth2.pojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class ErrorRoot {
+
+    @JsonProperty("error")
+    private Error error;
+
+    @JsonProperty("error")
+    public Error getError() {
+        return error;
+    }
+
+    @JsonProperty("error")
+    public void setError(Error error) {
+        this.error = error;
+    }
+
+}
